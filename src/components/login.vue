ff78bf535f561d6338dc94b0b1b045746e080b6e
5209691314Aa <474875346@qq.com>
2020-04-15 20:31:22 +0800

    operate

---
+++ src/components/login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="login_box"></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+.login_box{
+    width: 100px;
+    height: 100%;
+    background-color: red;
+}
+</style>
